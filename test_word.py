53b130e84f4e67e8226bdd39c950e199866ea3bf
Karthik Reddy <svb491@gmail.com>
2024-04-26 17:02:23 +0200

    1

---
+++ test_word.py
@@ -0,0 +1,38 @@
+from wordcounter import WordCounter
+
+wordcounter = WordCounter()
+
+
+def test_count_uppercase():
+    text = "Hello, World"
+    expected_result = 2
+    result = wordcounter.count_uppercase(text)
+    assert result == expected_result, f"Expected: {expected_result}, but got: {result}"
+
+
+def test_count_lowercase():
+    text = "Hello, World"
+    expected_result = 8
+    result = wordcounter.count_lowercase(text)
+    assert result == expected_result, f"Expected: {expected_result}, but got: {result}"
+
+
+def test_count_vowels():
+    text = "Hello, World"
+    expected_result = 3
+    result = wordcounter.count_vowels(text)
+    assert result == expected_result, f"Expected: {expected_result}, but got: {result}"
+
+
+def test_count_total_chars():
+    text = "Hello, World"
+    expected_result = 12
+    result = wordcounter.count_total_chars(text)
+    assert result == expected_result, f"Expected: {expected_result}, but got: {result}"
+
+
+def test_count_total_words():
+    text = "Hello, World vvvv"
+    expected_result = 3
+    result = wordcounter.count_total_words(text)
+    assert result == expected_result, f"Expected: {expected_result}, but got: {result}"
