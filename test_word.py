858c7bdd0755d726d4f28c724e7eb7688f463a1a
Karthik Reddy <svb491@gmail.com>
2024-04-27 16:22:34 +0200

    Now pushing in to the repositery with valid experted out put to make sure the test case passes

--- test_word.py
+++ test_word.py
@@ -5,7 +5,7 @@ wordcounter = WordCounter()
 # Test case for the uppercase count function
 def test_count_uppercase():
     text = "HEllo, World" # In put for the test
-    expected_result = 2 # Expected out put
+    expected_result = 3 # Expected out put
     result = wordcounter.count_uppercase(text) # Result for the test
     assert result == expected_result, f"Expected: {expected_result}, but got: {result}"
 
