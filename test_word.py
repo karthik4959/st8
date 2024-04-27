a7a591095b81be7cea97400023591dbbd9e7cbf0
Karthik Reddy <svb491@gmail.com>
2024-04-27 18:03:26 +0200

    now i am going to make all the test cases pass

--- test_word.py
+++ test_word.py
@@ -6,28 +6,28 @@ wordcounter = WordCounter()
 
 def test_count_uppercase():
     text = "HEllo, World" # In put for the test
-    expected_result = 1 # Expected out put
+    expected_result = 3 # Expected out put
     result = wordcounter.count_uppercase(text) # Result for the test
     assert result == expected_result, f"Expected: {expected_result}, but got: {result}"
 
 # Test case for the lowercase count function
 def test_count_lowercase():
     text = "Hello, World" # input for the test
-    expected_result = 2 # expected out put
+    expected_result = 8 # expected out put
     result = wordcounter.count_lowercase(text) # results for the test
     assert result == expected_result, f"Expected: {expected_result}, but got: {result}"
 
 
 def test_count_vowels():
     text = "Hello, World" # Input for the test
-    expected_result = 4 # expected out put for the test
+    expected_result = 3 # expected out put for the test
     result = wordcounter.count_vowels(text) # Result for the test
     assert result == expected_result, f"Expected: {expected_result}, but got: {result}" # print statement
 
 
 def test_count_total_chars():
     text = "Hello, World" # input for the test
-    expected_result = 1 # Expected output for the test
+    expected_result = 12 # Expected output for the test
     result = wordcounter.count_total_chars(text) # Results for the test case
     assert result == expected_result, f"Expected: {expected_result}, but got: {result}" # Print statement
 
