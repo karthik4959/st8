cc15da16dbe588b11b08715f9e5e7ce9e605b718
Karthik Reddy <svb491@gmail.com>
2024-04-27 18:01:33 +0200

    all tests fail

--- test_word.py
+++ test_word.py
@@ -3,35 +3,35 @@ from word import WordCounter
 wordcounter = WordCounter()
 
 # Test case for the uppercase count function
-"""
+
 def test_count_uppercase():
     text = "HEllo, World" # In put for the test
-    expected_result = 3 # Expected out put
+    expected_result = 1 # Expected out put
     result = wordcounter.count_uppercase(text) # Result for the test
     assert result == expected_result, f"Expected: {expected_result}, but got: {result}"
 
 # Test case for the lowercase count function
 def test_count_lowercase():
     text = "Hello, World" # input for the test
-    expected_result = 8 # expected out put
+    expected_result = 2 # expected out put
     result = wordcounter.count_lowercase(text) # results for the test
     assert result == expected_result, f"Expected: {expected_result}, but got: {result}"
 
 
 def test_count_vowels():
     text = "Hello, World" # Input for the test
-    expected_result = 3 # expected out put for the test
+    expected_result = 4 # expected out put for the test
     result = wordcounter.count_vowels(text) # Result for the test
     assert result == expected_result, f"Expected: {expected_result}, but got: {result}" # print statement
 
 
 def test_count_total_chars():
     text = "Hello, World" # input for the test
-    expected_result = 12 # Expected output for the test
+    expected_result = 1 # Expected output for the test
     result = wordcounter.count_total_chars(text) # Results for the test case
     assert result == expected_result, f"Expected: {expected_result}, but got: {result}" # Print statement
 
-"""
+
 def test_count_total_words():
     text = "Hello, World vvvv" # input for the test case
     expected_result = 3 # Input for the test case
--- word.py
+++ word.py
@@ -3,18 +3,18 @@ import re
 
 class WordCounter:
  #Method to count total number of uppercase letters in the given string
-   # def count_uppercase(self, text):
-    #    return sum(1 for char in text if char.isupper())
+    def count_uppercase(self, text):
+        return sum(1 for char in text if char.isupper())
 #Method to count total number of lowercase letters in the given string
-    #def count_lowercase(self, text):
-     #   return sum(1 for char in text if char.islower())
+    def count_lowercase(self, text):
+        return sum(1 for char in text if char.islower())
 # Method to count total number of vowels in teh given string
-    #def count_vowels(self, text):
-     #   vowels = 'aeiouAEIOU'
-      #  return sum(1 for char in text if char in vowels)
+    def count_vowels(self, text):
+        vowels = 'aeiouAEIOU'
+        return sum(1 for char in text if char in vowels)
 # MEthod to count total number of characters in the given string including spaces and special characters
-    #def count_total_chars(self, text):
-     #   return len(text)
+    def count_total_chars(self, text):
+        return len(text)
 # Method to count total number of words in the given string
     def count_total_words(self, text):
         words = re.findall(r'\b\w+\b', text)
