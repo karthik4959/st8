87492405c532232184d57507aed27939abbdb2db
Karthik Reddy <svb491@gmail.com>
2024-04-27 16:39:53 +0200

    Now i am giong to push the test with invalid expercted out put to make the test fail

--- word.py
+++ word.py
@@ -2,23 +2,20 @@ import re
 
 
 class WordCounter:
-#function to count the number of upper case letters in the given string
-"""
-    def count_uppercase(self, text):
-        return sum(1 for char in text if char.isupper())
-"""
-# Function to count total number of lower case letters in the given string
+ #Method to count total number of uppercase letters in the given string
+   # def count_uppercase(self, text):
+    #    return sum(1 for char in text if char.isupper())
+#Method to count total number of lowercase letters in the given string
     def count_lowercase(self, text):
         return sum(1 for char in text if char.islower())
-"""
-    def count_vowels(self, text):
-        vowels = 'aeiouAEIOU'
-        return sum(1 for char in text if char in vowels)
 
-    def count_total_chars(self, text):
-        return len(text)
+    #def count_vowels(self, text):
+     #   vowels = 'aeiouAEIOU'
+      #  return sum(1 for char in text if char in vowels)
 
-    def count_total_words(self, text):
-        words = re.findall(r'\b\w+\b', text)
-        return len(words)
-"""
+    #def count_total_chars(self, text):
+     #   return len(text)
+
+    #def count_total_words(self, text):
+     #   words = re.findall(r'\b\w+\b', text)
+      #  return len(words)
